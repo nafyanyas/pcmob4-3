4b923aadf0dc07555384e0de6e6d3c83ea4a5e00
adam.u <ubad.adam@gmail.com>
2022-07-16 16:47:48 +0800

    Chat Screen with Test Message

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,11 +1,52 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
+
+import { useNavigation } from "@react-navigation/native";
+import { GiftedChat } from "react-native-gifted-chat";
+import firebase from "../database/firebaseDB";
+const auth = firebase.auth();
+
+const demoMessage = {
+  _id: 1,
+  text: "Hello there!",
+  createdAt: new Date(),
+  user: {
+    _id: 2,
+    name: "Demo person",
+    avatar: "https://placeimg.com/140/140/any",
+  },
+};
 
 export default function ChatScreen() {
+  const [messages, setMessages] = useState([]);
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) navigation.navigate("Chat", { id: user.id, email: user.email });
+      else navigation.navigate("Login");
+    });
+
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={logout}>
+          <MaterialCommunityIcons name="logout" size={30} color="grey" />
+        </TouchableOpacity>
+      ),
+    });
+
+    setMessages([demoMessage]);
+  }, []);
+
+  const logout = () => auth.signOut();
+
   return (
-    <View>
-      <Text>ChatScreen</Text>
-    </View>
+    <GiftedChat
+      messages={messages}
+      listViewProps={{ style: { backgroundColor: "grey" } }}
+      user={{ _id: 1 }}
+    />
   );
 }
 
