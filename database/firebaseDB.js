a9a611c22309b167a5c61ed893e11baa7cdef072
adam.u <ubad.adam@gmail.com>
2022-07-16 14:44:49 +0800

    database file updated

--- database/firebaseDB.js
+++ database/firebaseDB.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app';
 import "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    authDomain: "xxxxxxxxx-00000.firebaseapp.com",
-    databaseURL: "https://xxxxxxxxx-00000.firebaseio.com",
-    projectId: "xxxxxxxxx-00000",
-    storageBucket: "xxxxxxxxx-00000.appspot.com",
-    messagingSenderId: "000000000000000",
-    appId: "1:000000000000000:web:000000000000000"
-};
+    apiKey: "AIzaSyCAy1fRvZd_h54N8XkG3w9phpcV5aOiqoQ",
+    authDomain: "pcmob4-1706f.firebaseapp.com",
+    projectId: "pcmob4-1706f",
+    storageBucket: "pcmob4-1706f.appspot.com",
+    messagingSenderId: "532949514341",
+    appId: "1:532949514341:web:00ee183ddfb354fdeb2e8c",
+    measurementId: "G-J4CP5JGEVM"
+  };
 
 firebase.initializeApp(firebaseConfig);
 export default firebase;
